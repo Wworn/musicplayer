1b7cb57fa7480c4a28bcb7d819f17806308f5839
Wworn <dedmause000@gmail.com>
2024-08-02 06:24:28 -0300

    Add files via upload

---
+++ music-es-termux.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Script creado por wr3nch
+
+clear
+
+confirm_install() {
+    echo "************************************"
+    echo "*  Componentes necesarios no       *"
+    echo "*  instalados. Desea instalarlos?  *"
+    echo "************************************"
+    read -p "Escriba 'si' para instalar: " confirm
+    if [[ "$confirm" == "si" ]]; then
+        # Actualizar e instalar yt-dlp y mpv
+        sudo apt update -y
+        sudo apt install -y mpv
+        sudo apt install -y yt-dlp
+        sudo apt install -y python
+        sudo apt install -y pip
+        # Para termux
+        pkg install mpv
+        pkg install python
+        pkg install pip
+        
+        pip install yt-dlp
+    else
+        echo "La instalación fue cancelada."
+        exit 1
+    fi
+}
+
+# Comprobar si yt-dlp y mpv están instalados
+if ! command -v yt-dlp &> /dev/null || ! command -v mpv &> /dev/null; then
+    confirm_install
+fi
+
+echo -e "\n\033[1;31m"
+echo "************************************"
+echo "*  BUSQUE LA MÚSICA QUE QUIERA:    *"
+echo "************************************"
+
+read -p "Ingrese su búsqueda: " search_query
+
+search_query=$(echo "$search_query" | sed 's/ /+/g')
+
+# Ejecutar la búsqueda y reproducción (Necesarios yt-dpl y mpv)
+yt-dlp "ytsearch:$search_query" --no-playlist -f bestaudio -o - | mpv --no-video -
